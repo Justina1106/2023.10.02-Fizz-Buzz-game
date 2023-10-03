6470046f2bb8f65271f26e27c8ada47355181728
Justina1106 <jbycenkova@gmail.com>
2023-10-03 18:56:35 +0300

    Pridėti mokytojo sutrumpinti variantai

--- script.js
+++ script.js
@@ -30,48 +30,146 @@
 // 7. Skaičiai, kurie dalinasi iš 11, turi būti pakeisti į žodį „Bizz".
 
 
-function FizzBuzz() {
-    let result = [];
-    for (let i = 1; i <= 100; i++) {
-        if (i % 7 === 0 && i % 5 === 0 && i % 3 === 0) {
-            result.push("FizzBuzzBiff");
-        }
-        else if (i % 3 === 0) {
-            result.push("Fizz");
-        }
-        else if (i % 5 === 0) {
-            result.push("Buzz");
-        }
-        else if (i % 3 === 0 && i % 5 === 0) {
-            result.push("FizzBuzz");
-        }
-        else if (i % 7 === 0) {
-            result.push("Biff");
-        }
-        else if (i % 3 === 0 && i % 7 === 0) {
-            result.push("FizzBiff");
-        }
-        else if (i % 5 === 0 && i % 7 === 0) {
-            result.push("BuzzBiff");
-        }
-        else if (i % 9 === 0) {
-            result.push("Fuzz");
-        }
-        else if (i % 11 === 0) {
-            result.push("Bizz");
-        }
-        else {
-            result.push(i.toString());
-        }
+// function FizzBuzz() {
+//     let result = [];
+//     for (let i = 1; i <= 1000; i++) {
+//         if (i % 7 === 0 && i % 5 === 0 && i % 3 === 0) {
+//             result.push("FizzBuzzBiff");
+//         }
+//         else if (i % 3 === 0 && i % 5 === 0) {
+//             result.push("FizzBuzz");
+//         }
+//         else if (i % 3 === 0 && i % 7 === 0) {
+//             result.push("FizzBiff");
+//         }
+//         else if (i % 5 === 0 && i % 7 === 0) {
+//             result.push("BuzzBiff");
+//         }
+//         else if (i % 3 === 0) {
+//             result.push("Fizz");
+//         }
+//         else if (i % 5 === 0) {
+//             result.push("Buzz");
+//         }
+//         else if (i % 7 === 0) {
+//             result.push("Biff");
+//         }
+//         else if (i % 9 === 0) {
+//             result.push("Fuzz");
+//         }
+//         else if (i % 11 === 0) {
+//             result.push("Bizz");
+//         }
+//         else {
+//             result.push(i.toString());
+//         }
+//     }
+//     return result;
+// }
+ 
+
+// let n = 100;
+ 
+
+// // let result = FizzBuzz(n);
+ 
+
+// // console.log(result.join(' '));
+
+function fizzBuzz2() {
+    for (let i = 1; i <= 1000; i++) {
+
+    let output = ''
+
+    if (i % 3 === 0 && i % 5 === 0 && i % 7 === 0 && i % 9 === 0) {
+        output = 'FizzBuzzBiffFuzz'
+    } else if (i % 3 === 0 && i % 5 === 0 && i % 7 === 0) {
+        output = 'FizzBuzzBiff'
+    } else if (i % 3 === 0 && i % 5 === 0 && i % 9 === 0) {
+        output = 'FizzBuzzFuzz'
+    } else if (i % 3 === 0 && i % 7 === 0 && i % 9 === 0) {
+        output = 'FizzBiffFuzz'
+    } else if (i % 5 === 0 && i % 7 === 0 && i % 9 === 0) {
+        output = 'BuzzBiffFuzz'
+    } else if (i % 3 === 0 && i % 5 === 0) {
+        output = 'FizzBuzz'
+    } else if (i % 3 === 0 && i % 7 === 0) {
+        output = 'FizzBiff'
+    } else if (i % 3 === 0 && i % 9 === 0) {
+        output = 'FizzFuzz'
+    } else if (i % 5 === 0 && i % 7 === 0) {
+        output = 'BuzzBiff'
+    } else if (i % 5 === 0 && i % 9 === 0) {
+        output = 'BuzzFuzz'
+    } else if (i % 7 === 0 && i % 9 === 0) {
+        output = 'BiffFuzz'
+    } else if (i % 3 === 0) {
+        output = 'Fizz'
+    } else if (i % 5 === 0) {
+        output = 'Buzz'
+    } else if (i % 7 === 0) {
+        output = 'Biff'
+    } else if (i % 9 === 0) {
+        output = 'Fuzz'    
+    } else {
+        output = i
     }
-    return result;
+    console.log(output)
+    }
 }
- 
 
-let n = 100;
- 
+    // fizzBuzz2()
 
-let result = FizzBuzz(n);
- 
+    function fizzBuzz3() {
+        for (let i = 1; i <= 1000; i++) {
+      
+          let output = ''
+      
+          if (i % 3 === 0) {
+            output += 'Fizz'
+          }
+      
+          if (i % 5 === 0) {
+            output += 'Buzz'
+          }
+      
+          if (i % 7 === 0) {
+            output += 'Biff'
+          }
+      
+          if (i % 9 === 0) {
+            output += 'Fuzz'
+          }
+      
+          if (i % 11 === 0) {
+            output += 'Bizz'
+          }
+      
+          if (!output) {
+            output = i
+          }
+      
+          console.log(output)
+      
+        }
+      }
+      
+       fizzBuzz3()
 
-console.log(result.join(' '));
+    function fizzBuzz4() {
+        for (let i = 1; i <= 1000; i++) {
+      
+          let output = ''
+      
+          if (i % 3 === 0) output += 'Fizz'
+          if (i % 5 === 0) output += 'Buzz'
+          if (i % 7 === 0) output += 'Biff'
+          if (i % 9 === 0) output += 'Fuzz'
+          if (i % 11 === 0) output += 'Bizz'
+          if (!output) output = i
+      
+          console.log(output)
+      
+        }
+      }
+    //   fizzBuzz4()
